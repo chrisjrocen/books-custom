ea3a29e7ed9181cd965ee42752be1487bfc8e105
chrisocen <105217816+chrisocen@users.noreply.github.com>
2022-06-07 12:52:48 +0300

    wpcs fixes

--- core/taxonomies/genres.php
+++ core/taxonomies/genres.php
@@ -1,32 +1,31 @@
 <?php
 /**********************************************************************************
-	 register taxonomy - genre
+	Register taxonomy - genre
  **********************************************************************************/
-function chrx_register_taxonomy()
-{
+function chrx_register_taxonomy() {
 	// genre
 	$labels = array(
-		'name'              => __('genres'),
-		'singular_name'     => __('genre'),
-		'search_items'      => __('Search genres'),
-		'all_items'         => __('All genres'),
-		'edit_item'         => __('Edit genre'),
-		'update_item'       => __('Update genre'),
-		'add_new_item'      => __('Add New genre'),
-		'new_item_name'     => __('New genre Name'),
-		'menu_name'         => __('genres'),
+		'name'              => __( 'Genres' ),
+		'singular_name'     => __( 'Genre' ),
+		'search_items'      => __( 'Search genres' ),
+		'all_items'         => __( 'All genres' ),
+		'edit_item'         => __( 'Edit genre' ),
+		'update_item'       => __( 'Update genre' ),
+		'add_new_item'      => __( 'Add New genre' ),
+		'new_item_name'     => __( 'New genre Name' ),
+		'menu_name'         => __( 'Genres' ),
 	);
 
 	$args = array(
-		'labels' => $labels,
-		'hierarchical' => true,
-		'sort' => true,
-		'args' => array('orderby' => 'term_order'),
-		'rewrite' => array('slug' => 'genres'),
+		'labels'            => $labels,
+		'hierarchical'      => true,
+		'sort'              => true,
+		'args'              => array( 'orderby' => 'term_order' ),
+		'rewrite'           => array( 'slug' => 'genres' ),
 		'show_admin_column' => true,
-		'show_in_rest' => true
+		'show_in_rest'      => true,
 	);
 
-	register_taxonomy('chrx_genre', array('chrx_book'), $args);
+	register_taxonomy( 'chrx_genre', array( 'chrx_book' ), $args );
 }
-add_action('init', 'chrx_register_taxonomy');
+add_action( 'init', 'chrx_register_taxonomy' );
