140895a73702beda6d8a39899f4def177f8348a8
chrisocen <105217816+chrisocen@users.noreply.github.com>
2022-06-07 12:39:03 +0300

    added wpcs

--- core/shortcodes/books.php
+++ core/shortcodes/books.php
@@ -16,27 +16,6 @@ function register_chrx_books_shortcode() {
 
 add_action( 'init', 'register_chrx_books_shortcode' );
 
-// add_action( 'wp_enqueue_scripts', 'enqueue_scripts', 10 );
-// function enqueue_scripts() {
-// 	wp_enqueue_style( 'movies-css', plugin_dir_url( dirname( __FILE__, 2 ) ) . 'assets/build/css/base.css', array(), '0.1.0', 'all' );
-// 	wp_enqueue_script( 'movies-js', plugin_dir_url( dirname( __FILE__, 2 ) ) . 'assets/build/js/base.js', ['jquery'], '0.1.0', true );
-// 	wp_localize_script(
-// 		'books-js',
-// 		'books_ajax',
-// 		array(
-// 			'ajax_url' => admin_url( 'admin-ajax.php' ),
-// 		)
-// 	);
-// }
-
-// add_action( 'wp_ajax_filter_movies', 'leisure_tym_filter_movies' );
-// add_action( 'wp_ajax_nopriv_filter_movies', 'leisure_tym_filter_movies' );
-// function leisure_tym_filter_movies() {
-// 	echo wp_send_json('yes it works');
-
-// 	wp_die();
-// }
-
 /**
  * Query and display movies
  *
